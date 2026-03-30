93ce7c205641f1b0dd80ab7c9f4f2f742287df55
ameyg24 <amey.gaba24@gmail.com>
2026-03-30 09:30:25 +0900

    Add: design-twitter-feed - submission-10

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-10.py
@@ -0,0 +1,33 @@
+import heapq
+class Twitter:
+
+    def __init__(self):
+        self.time = 0
+        self.maps = collections.defaultdict(list)
+        self.followers = collections.defaultdict(list)
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.maps[userId].append((tweetId, self.time))
+        self.time += 1
+        if userId not in self.followers[userId]:
+            self.followers[userId].append(userId)
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        heap = []
+        for el in self.followers[userId]:
+            for tweet, time in self.maps[el]:
+                heap.append((time, tweet))
+        heapq.heapify(heap)
+        while len(heap) > 10:
+            heapq.heappop(heap)
+        print(heap)
+        heap.sort(reverse = True)
+        return [tweet for time, tweet in heap]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followeeId not in self.followers[followerId] and followeeId != followerId:
+            self.followers[followerId].append(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.followers[followerId] and followeeId != followerId:
+            self.followers[followerId].remove(followeeId)
