a1f7ef235764d9174bbd5e774880d16d8839cddb
ameyg24 <amey.gaba24@gmail.com>
2026-05-21 22:53:55 -0500

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [(False,float('inf'))] * (n)
+        dp[-1] = (True,0)
+        for i in range(n-2,-1,-1):
+            for j in range(1,nums[i] + 1,1):
+                if i + j <= n-1 and dp[i+j][0]:
+                    dp[i] = (True, min(1 + dp[i+j][1], dp[i][1]))
+        print(dp)
+        return dp[0][1]
