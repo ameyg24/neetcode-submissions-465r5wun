72bf963eae44d57fdd266c69dceea258bbe7f880
ameyg24 <amey.gaba24@gmail.com>
2026-07-08 21:21:32 +0900

    Add: network-delay-time - submission-4

---
+++ Data Structures & Algorithms/network-delay-time/submission-4.py
@@ -0,0 +1,17 @@
+import heapq
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        minheap = []
+        minheap.append((0,k))
+        dictionary = {}
+        cost = 0
+        while minheap:
+            dist, node = heapq.heappop(minheap)
+            if node not in dictionary:
+                print(dist, node)
+                cost = dist
+                dictionary[node] = dist
+                for src, tar, time in times:
+                    if src == node:
+                        heapq.heappush(minheap, (time + dist, tar))
+        return cost if len(dictionary) == n else -1
